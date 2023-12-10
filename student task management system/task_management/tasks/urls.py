b5b80cd3b705a45c319b401043ee0329d5fc5a24
zarghaampathan <153376056+zarghaampathan@users.noreply.github.com>
2023-12-10 08:14:52 +0500

    Add files via upload

---
+++ student task management system/task_management/tasks/urls.py
@@ -0,0 +1,14 @@
+# tasks/urls.py
+from django.urls import path
+from . import views
+from .views import task_list
+
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/new/', views.task_new, name='task_new'),
+    path('task/<int:pk>/edit/', views.task_edit, name='task_edit'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
+    path('', task_list, name='task_list'),
+]
---
+++ student task management system/task_management/tasks/views.py
@@ -0,0 +1,42 @@
+# tasks/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Task
+from .forms import TaskForm
+from django.utils import timezone
+
+def task_list(request):
+    #tasks = Task.objects.filter(deadline__gte=timezone.now()).order_by('deadline')
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'tasks/task_detail.html', {'task': task})
+
+def task_new(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_edit.html', {'form': form})
+
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_edit.html', {'form': form})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('task_list')
