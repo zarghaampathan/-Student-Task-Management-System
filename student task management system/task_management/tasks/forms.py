bf6f903d2990618bceb2d2e56575db2855a1780d
zarghaampathan <153376056+zarghaampathan@users.noreply.github.com>
2023-12-10 10:05:24 +0500

    Delete student task management system/task_management directory

--- student task management system/task_management/tasks/forms.py
+++
@@ -1,8 +0,0 @@
-# tasks/forms.py
-from django import forms
-from .models import Task
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ['title', 'description', 'deadline']
